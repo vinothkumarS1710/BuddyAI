6c6e087d5bc738e655c07ca4672ff8c6cbac1cac
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-05-18 22:30:32 +0530

    buildSideBar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -110,7 +110,7 @@ const SideBar = ({ isMenuOpen, setIsMenuOpen }) => {
       </div>
 
       <div className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group">
-        <img src={assets.user_icon} className="w-7 rounded-full" />
+        <img src={assets.user_icon} className="w-5 rounded-full" />
         <p className="flex-1 text-sm dark:text-primary truncate">
           {user ? user.name : "Login your account"}
         </p>
