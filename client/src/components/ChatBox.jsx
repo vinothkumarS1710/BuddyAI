17bdc1031d00b7383bb8760bf7b8123bda647066
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-05-19 11:00:52 +0530

    buildStructureAndSideBar

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AppContext'
 
 const ChatBox = () => {
+  const {selectedChats, theme} = useAppContextntext()
+
+  const [messages, setMessages] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+      if(selectedChats){
+        setMessages(selectedChats.messages)
+      }
+  },[selectedChats])
   return (
     <div>
       ChatBox
