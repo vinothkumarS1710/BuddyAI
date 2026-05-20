cf37cc2f41613bc3a2facd20b1d02d0fd0b89f83
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-05-20 11:26:48 +0530

    buildCommunityAndLoadingPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import './assets/prism.css'
 const App = () => {
   
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  
 
   return (
     <>
--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,9 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ChatBox from "../components/ChatBox";
 import Credits from '../pages/Credits'
 import Community from "../pages/Community";
+import Loading from "../pages/Loading";
 
 const AllRoutes = () => {
+
+  const {pathname} = useLocation()
+
+  if(pathname === '/loading') return <Loading/>
+
   return (
     <Routes>
       <Route path="/" element={<ChatBox />} />
