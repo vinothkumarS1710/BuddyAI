cffd8869aa3a0773bea02d4b3fb2c88116cb47af
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-05-22 19:30:15 +0530

    authMiddleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+
+export const protect = async (req, res, next) => {
+    let token = req.headers.authorization;
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decoded.id;
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.json({success: false, message: "Not authorized, user not found"});
+        }
+        req.user = user;
+        next()
+    } catch (err) {
+        res.status(401).json({message: "Not authorized, token failed"})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import connectDB from './configs/db.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const app = express()
@@ -13,6 +14,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) =>res.send('Server is Live!'))
+app.use('/api/user', userRouter)
+
 
 const PORT = process.env.PORT || 3000
 
